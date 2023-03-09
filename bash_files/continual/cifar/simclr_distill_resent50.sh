ced3392cf33e8eda5728457b4be68a32e32ae424
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2023-03-09 14:24:24 +0800

    official

---
+++ bash_files/continual/cifar/simclr_distill_resent50.sh
@@ -0,0 +1,37 @@
+python3 main_continual.py \
+    --dataset cifar100 \
+    --encoder resnet50 \
+    --data_dir $DATA_DIR \
+    --split_strategy class \
+    --task_idx 0 \
+    --max_epochs 500 \
+    --num_tasks 5 \
+    --gpus 0 \
+    --precision 16 \
+    --optimizer sgd \
+    --lars \
+    --grad_clip_lars \
+    --eta_lars 0.02 \
+    --exclude_bias_n_norm \
+    --scheduler warmup_cosine \
+    --lr 0.4 \
+    --classifier_lr 0.1 \
+    --weight_decay 1e-5 \
+    --batch_size 256 \
+    --num_workers 5 \
+    --brightness 0.8 \
+    --contrast 0.8 \
+    --saturation 0.8 \
+    --hue 0.2 \
+    --gaussian_prob 0.0 0.0 \
+    --name simclr-cifar100-contrastive \
+    --project semi-ISSL \
+    --entity pigpeppa \
+    --wandb \
+    --save_checkpoint \
+    --method simclr \
+    --temperature 0.2 \
+    --proj_hidden_dim 2048 \
+    --output_dim 256 \
+    --distiller contrastive \
+    --disable_knn_eval
